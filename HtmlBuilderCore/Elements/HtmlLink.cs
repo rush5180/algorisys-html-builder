bf252aa58d4e2ec192acf3c0bb6869a89c07facd
Gideon Israel Dsouza <gideon.dsouza@algorisys.com>
2017-06-16 12:16:55 +0530

    Added all files.

---
+++ HtmlBuilderCore/Elements/HtmlLink.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlBuilderCore.Elements
+{
+    public class HtmlLink : IHtmlElement
+    {
+        public string LinkText { get; set; }
+        public string LinkSrc { get; set; }
+
+        public string GetHtmlString()
+        {
+            return string.Format("<a href='{0}'>{1}</a>", this.LinkSrc, this.LinkText);
+        }
+    }
+}
